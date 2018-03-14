d9c8b663e1cd33520b445a317ee033de4a36476c
Mitch Lembke <mitchell.lembke@gmail.com>
2018-03-14 11:22:54 -0600

    Refactored

--- serverExpress.js
+++ serverExpress.js
@@ -1,15 +1,20 @@
 'use strict';
+
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
 
 app.disable('x-powered-by');
 
-app.use(function(req, res) {
+app.get('/guests', function(req, res) {
   var guests = ['Mary', 'Don'];
   res.send(guests);
 });
 
+app.use(function(req, res) {
+  res.sendStatus(404);
+});
+
 app.listen(port, function() {
   console.log('Listening on port', port);
-});
+}); 
